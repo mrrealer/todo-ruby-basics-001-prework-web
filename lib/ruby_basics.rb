2d1abe3127033fc083c6f3a6e3ed027108b4e302
mrrealer <dwayne.samuel@gmail.com>
2016-03-14 21:42:10 -0400

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,35 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
+  return value
 end
 
-def argue
-  
+
+def argue(phrase)
+  phrase = "I'm right and you are wrong!"
+  return phrase
 end
 
-def greeting
+def greeting(phrase, name)
+  puts "Hello", "#{"Dwayne"}"
 
 end
 
 def return_a_value
-
+  phrase = "Nice"
+  return phrase
 end
+return_a_value
 
 def last_evaluated_value
+  phrase = "expert"
 
 end
-
-def pizza_party
-
+last_evaluated_value
+def pizza_party(phrase="cheese")
+  return phrase
 end
+pizza_party
